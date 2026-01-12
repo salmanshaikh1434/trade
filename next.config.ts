98690b527f5f2b754af3c841305a224d7c5b897a
salman shaikh <salman@techmaticsys.com>
2026-01-12 13:24:10 +0530

    resolve env issue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    /* config options here */
+    env: {
+        GROWW_API_KEY: process.env.GROWW_API_KEY,
+        GROWW_API_SECRET: process.env.GROWW_API_SECRET,
+    },
+    output: 'standalone',
 };
 
 export default nextConfig;
